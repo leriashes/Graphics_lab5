a302d9c77dfe8f48955337502c11cf92d6964917
Valeria Shevchenko <valeriya-shevchenko-2002@mail.ru>
2023-12-05 16:47:18 +0700

    Отсутствует график

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
 
 namespace Graphics_lab5
 {
